9e9379edae5ef0fbd29ff011578ffd9f670357c7
seleniumstar <msudhir29@gmail.com>
2020-02-29 14:05:45 -0500

    added searchpage class

---
+++ src/main/java/com/qa/pages/SearchPage.java
@@ -0,0 +1,14 @@
+package com.qa.pages;
+
+import org.testng.annotations.Test;
+
+public class SearchPage {
+	
+	@Test
+	public void searchpageMethod()
+	{
+		System.out.println("testing search page");
+	}
+
+
+}
