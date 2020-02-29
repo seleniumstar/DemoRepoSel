4f7555f3257e8796518e460d1b65a0009b5ac024
seleniumstar <msudhir29@gmail.com>
2020-02-29 14:21:06 -0500

    commiting submit page

---
+++ src/main/java/com/qa/pages/SubmitPage.java
@@ -0,0 +1,13 @@
+package com.qa.pages;
+
+import org.testng.annotations.Test;
+
+public class SubmitPage {
+	
+	@Test
+	public void submitMethod()
+	{
+		System.out.println("testing submit page");
+	}
+
+}
